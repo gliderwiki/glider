782742fdf0353cfa85886c2d35efd413210fdec5
gliderwiki <performizer@gmail.com>
2013-03-05 17:30:44 +0900

    공간접근정보 권한 지정

--- WebContent/resource/libs/plugin/jquery-user-list-layer.js
+++ WebContent/resource/libs/plugin/jquery-user-list-layer.js
@@ -3,53 +3,47 @@
 	/**
 	 * 사용자 프로필 레이어 팝업을 출력한다. 
 	 */
-	jQuery.groupInfoLayer = function(params){
+	jQuery.userListLayer = function(params){
 		
-		var list = params.weGroupList;
+		var list = params.userList;
 		var checkType = params.type;
 		var authorityType  = params.authorityType;
-		var listSize = params.weGroupList.length;
+		var listSize = params.userList.length;
 		
 		if($('#groupWrap').length){
 			return false;
 		}
-		var group_type = "";
+		
 		var resourceHtml = "";
 			resourceHtml = "<div class=\"groupWrap\" id=\"groupWrap\">";
 			resourceHtml	+= "<div class=\"loadingBg\"></div>";
 			resourceHtml	+= "<div id=\"groupDiv\">"; 
 			resourceHtml	+= "<section class=\"layer-select\">";
-			resourceHtml	+= "<div class=\"tit\"><strong>그룹을 선택하세요</strong></div>";
+			resourceHtml	+= "<div class=\"tit\"><strong>구성원을 선택하세요</strong></div>";
 			resourceHtml	+= "<div class=\"cont\">";
 			resourceHtml	+= "	<table>";
 			resourceHtml	+= "	<thead>";
 			resourceHtml	+= "		<tr>";
-			resourceHtml	+= "			<th class=\"name\" style=\"width:240px\">그룹명</th>";
-			resourceHtml	+= "			<th class=\"name\" style=\"width:100px\">그룹관리자</th>";
-			resourceHtml	+= "			<th class=\"name\" style=\"width:160px\">그룹타입</th>";
-			resourceHtml	+= "			<th class=\"name\" style=\"width:100px\">그룹개설일</th>"; 
+			resourceHtml	+= "			<th class=\"name\" style=\"width:240px\">회원아이디</th>";
+			resourceHtml	+= "			<th class=\"name\" style=\"width:100px\">닉네임</th>";
+			resourceHtml	+= "			<th class=\"name\" style=\"width:160px\">가입일</th>";
+			resourceHtml	+= "			<th class=\"name\" style=\"width:100px\">이름</th>"; 
 			resourceHtml	+= "		</tr>";
 			resourceHtml	+= "	</thead>";
 			resourceHtml	+= "	<tbody>";
 			for (var i = 0 ; i < listSize ; i++){
-				
-				if(list[i].we_group_type == 0) {
-					group_type = "조직";
-				} else {
-					group_type = "사용자 생성 그룹";
-				}
 				resourceHtml	+= "		<tr>";
-				resourceHtml	+= "			<td class=\"first\"><input type=\"checkbox\" name=\"group_idx\" value=\""+list[i].we_group_idx+"\" id=\"check_"+list[i].we_group_idx+"\"  title=\""+list[i].we_group_name+"\" />"+list[i].we_group_name+"</td>";
+				resourceHtml	+= "			<td class=\"first\"><input type=\"checkbox\" name=\"user_idx\" value=\""+list[i].we_user_idx+"\" id=\"check_"+list[i].we_user_idx+"\"  title=\""+list[i].we_user_nick+"\" />"+list[i].we_user_id+"</td>";
 				resourceHtml	+= "			<td class=\"name\">"+list[i].we_user_nick+"</td>";
-				resourceHtml	+= "			<td class=\"name\">"+group_type+"</td>";
-				resourceHtml	+= "			<td class=\"name\">"+$.format.date(list[i].we_ins_date, "yyyy.MM.dd")+"</td>";
+				resourceHtml	+= "			<td class=\"name\">"+$.format.date(list[i].we_user_join_date, "yyyy.MM.dd")+"</td>";
+				resourceHtml	+= "			<td class=\"name\">"+list[i].we_user_name+"</td>";
 				resourceHtml	+= "		</tr>";
 			}
 			resourceHtml	+= "	</tbody>";
 			resourceHtml	+= "	</table>";
 			resourceHtml	+= "</div>";
 			resourceHtml	+= "<div class=\"wrap-btn\">";
-			resourceHtml	+= "	<a style=\"cursor:pointer\" role=\"button\" id=\"checkGroupId\" class=\"btn\" title=\""+authorityType+"\">선택완료</button>";
+			resourceHtml	+= "	<a style=\"cursor:pointer\" role=\"button\" id=\"checkUserId\" class=\"btn\" title=\""+authorityType+"\">선택완료</button>";
 			resourceHtml	+= "	<a style=\"cursor:pointer\" role=\"button\" class=\"btn\" id=\"close_btn\">닫기</a>";
 			resourceHtml	+= "</div>";
 			resourceHtml	+= "</section>";
@@ -61,7 +55,7 @@
 		wrapWindowMask();
 	};
 	
-	$.groupInfoLayer.hide = function(){
+	$.userListLayer.hide = function(){
 		$('#groupWrap').fadeOut(function(){
 			$(this).remove();
 			return false;
@@ -69,14 +63,14 @@
 	};
 	
 	$('#close_btn').live("click" , function(e) {
-		$.groupInfoLayer.hide();
+		$.userListLayer.hide();
 	});
-	$('#checkGroupId').live("click" , function(e) {
-		var resultData = $('input[name=group_idx]:checked').map(function() {
+	$('#checkUserId').live("click" , function(e) {
+		var resultData = $('input[name=user_idx]:checked').map(function() {
 		    return this.value;			
 		}).get().join(',');
 		
-		var resultName = $('input[name=group_idx]:checked').map(function() {
+		var resultName = $('input[name=user_idx]:checked').map(function() {
 			return this.title;			
 		}).get().join(',');
 		
@@ -84,16 +78,16 @@
 		console.log("resultName : " + resultName);
 		var authorityType = $(this).attr("title"); 
 		if(resultData == '' || resultData == null) {
-			GliderWiki.alert("알림","그룹을 선택하세요"); 
+			GliderWiki.alert("알림","구성원을 선택하세요"); 
 			return;
 		}
 		
 		console.log("authorityType : "  +authorityType);
 		
-		if(authorityType == 'view') {	// 조회 권한 
+		if(authorityType == 'view') {	// 조회권한 
 			WeSpace.we_view_data.value = resultData;
-			WeSpace.we_view_name.value = resultName;
-		} else {	// 수정 권한 
+			WeSpace.we_view_name.value = resultName;			
+		} else {	// 수정권한 
 			WeSpace.we_edit_data.value = resultData;
 			WeSpace.we_edit_name.value = resultName;
 		}
@@ -101,7 +95,8 @@
 		console.log("#authorityType : " +authorityType);
 		console.log($("#we_view_data").val());
 		console.log($("#we_view_name").val());
-		$.groupInfoLayer.hide();
+		$.userListLayer.hide();
+		
 	});
 	
 	function wrapWindowMask(){
@@ -112,4 +107,10 @@
 		$("#groupDiv").css('top',  POSTOP);         
 		$("#groupDiv").css('left', POSLEFT);		
 	}
+	
+		
 })(jQuery);
+
+
+
+
