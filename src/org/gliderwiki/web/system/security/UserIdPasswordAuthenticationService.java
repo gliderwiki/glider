3dd4342930929485e06a14ac114a9f6f4819fea8
gliderwiki <performizer@gmail.com>
2013-04-02 17:49:34 +0900

    어드민 수정

--- src/org/gliderwiki/web/system/security/UserIdPasswordAuthenticationService.java
+++ src/org/gliderwiki/web/system/security/UserIdPasswordAuthenticationService.java
@@ -19,6 +19,8 @@ import org.gliderwiki.framework.util.StringUtil;
 import org.gliderwiki.web.domain.WeUser;
 import org.gliderwiki.web.login.service.LoginService;
 import org.gliderwiki.web.vo.MemberSessionVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Service;
@@ -31,6 +33,8 @@ import org.springframework.stereotype.Service;
 @Service(value = "authenticationService")
 public class UserIdPasswordAuthenticationService implements AuthenticationService {
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Resource(name = "loginService")
 	LoginService loginService;
 
@@ -39,6 +43,10 @@ public class UserIdPasswordAuthenticationService implements AuthenticationServic
 
 	@Override
 	public void login(String userId, String password) throws Throwable, UserNotFoundException, AuthenticationNotException, 	PasswordMismatchException {
+		logger.debug("#####################################");
+		logger.debug("여긴 언제 들어오는거냐~~~");
+		logger.debug("#####################################");
+		
 		if (StringUtils.isEmpty(userId)) {
 			throw new UserNotFoundException(messages.getMessage("account.authenticationFailure"));
 		}
