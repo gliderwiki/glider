06cbbda197eaca96ce6c049fbf21781ea9e8094d
songanji <songanji@gmail.com>
2013-04-07 13:55:41 +0900

    이미지태그 수정

--- WebContent/resource/glider/wiki/customer/fn-layer-image.js
+++ WebContent/resource/glider/wiki/customer/fn-layer-image.js
@@ -183,7 +183,7 @@
 					}
 					//[!|http://asdf.com|aaa][!a|left][!s|30|40][!]
 					var imgLink = G_IMAGE._tag._align + ":" +  _align + G_IMAGE._tag._post
-								+ _align + G_IMAGE._tag._pre + G_IMAGE._tag._split + _url + G_IMAGE._tag._split + _title + G_IMAGE._tag._post
+								+ G_IMAGE._tag._pre + G_IMAGE._tag._split + _url + G_IMAGE._tag._split + _title + G_IMAGE._tag._post
 								+ G_IMAGE._tag._size + G_IMAGE._tag._split + _width + G_IMAGE._tag._split + _height + G_IMAGE._tag._post
 								+ G_IMAGE._tag._pre + G_IMAGE._tag._post
 								+ G_IMAGE._tag._align + G_IMAGE._tag._post ;
